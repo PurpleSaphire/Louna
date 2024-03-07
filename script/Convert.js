4d34baf2c5c19e8823589bff1569628f9605bb76
PurpleSaphire <162503387+PurpleSaphire@users.noreply.github.com>
2024-03-07 04:44:20 +0100

    Update Convert.js

--- script/Convert.js
+++ script/Convert.js
@@ -1,11 +1,11 @@
 module.exports.config = {
-  name: 'convert',
+  name: 'convertcmd',
   version: '1.0.0',
   role: 0,
   hasPrefix: false,
   aliases: [],
   description: 'Converts a command to the structure of the "parole" command.',
-  usage: 'convert [command]',
+  usage: 'convertcmd [command]',
   credits: 'MarianCross',
   cooldown: 0,
 };
@@ -18,30 +18,21 @@ module.exports.run = async function({ api, event, args }) {
 
   try {
     const code = args.join(' ');
-    const context = {
-      axios: require('axios'),
-      fs: require('fs'),
-      path: require('path'),
-      global: { utils: { getStreamFromURL: () => {} } }, // Mocked function for global.utils.getStreamFromURL
-      api,
-      event,
-      message: { reply: () => {} }, // Mocked reply function
-    };
     const command = eval(`(${code})`);
 
     const convertedCommand = {
       config: {
-        name: command.config.name,
-        version: command.config.version,
-        role: command.config.role,
-        hasPrefix: command.config.hasPrefix,
-        aliases: command.config.aliases,
-        description: command.config.description,
-        usage: command.config.usage,
-        credits: command.config.credits,
-        cooldown: command.config.cooldown,
+        name: command.config.name || '',
+        version: command.config.version || '',
+        role: command.config.role || 0,
+        hasPrefix: command.config.hasPrefix || false,
+        aliases: command.config.aliases || [],
+        description: command.config.description || '',
+        usage: command.config.usage || '',
+        credits: command.config.credits || '',
+        cooldown: command.config.cooldown || 0,
       },
-      run: command.run.toString(),
+      run: command.run.toString() || '',
     };
 
     api.sendMessage(JSON.stringify(convertedCommand, null, 2), event.threadID, event.messageID);
